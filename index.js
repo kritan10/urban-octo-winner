afb3994eb31ffaf88ec1091bf939b89ec2185ac8
kritan.thapa <kritan.thapa@swifttech.com.np>
2024-06-24 14:52:32 +0545

    added authentication feature

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import sqlitePlugin from 'fastify-sqlite-typed';
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import dotenv from 'dotenv';
+import middie from '@fastify/middie';
 
 dotenv.config();
 
@@ -16,9 +17,23 @@ await fastify.register(cors, {
   origin: '*',
 });
 
-fastify.register(sqlitePlugin, {
+await fastify.register(sqlitePlugin, {
   dbFilename: './db.sqlite',
 });
+
+await fastify.register(middie);
+
+const authPreHandler = (request, reply, done) => {
+  const auth = request.headers.authorization;
+  if (!auth || !auth.startsWith('Basic ')) {
+    return reply.status(401).send({ status: 200, message: 'Invalid authentication' });
+  }
+  const [username, password] = atob(auth.split(' ')[1]).split(':');
+  if (username !== 'Secret_Username' || password !== 'Secret_Password') {
+    return reply.status(401).send({ status: 401, message: 'Invalid credentials' });
+  }
+  done();
+};
 
 const db = new sqlite.Database('./db.sqlite');
 db.run(`CREATE TABLE IF NOT EXISTS transactions (
@@ -38,12 +53,14 @@ const SUSPICIOUS_CODE = 102;
 const CODES = [SUCCESS_CODE, FAILURE_CODE, SUSPICIOUS_CODE];
 
 fastify.get('/docs', async function handler(request, reply) {
-  await fastify;
   return {
     endpoints: [
+      { path: '/health', method: 'GET', info: 'check if the API is working' },
+      { path: '/get-credentials', method: 'POST', info: 'get credentials for third party api. use basic authentication i.e. Username/Password for subsequent requests' },
       {
         path: '/make-payment',
         method: 'POST',
+        info: 'the main functinoality of this api. will store the txn details in sqlite db',
         requestBody: {
           userId: 'bed66608-7b7f-4772-b646-b89cb6d7dc6b //must be uuid',
           toAccountNumber: '1092340293840 //cannot be same',
@@ -51,12 +68,9 @@ fastify.get('/docs', async function handler(request, reply) {
           amount: '100 //must be greater than 100',
         },
       },
-      { path: '/get-transaction-details', method: 'GET' },
-      { path: '/health', method: 'GET' },
-      {
-        statusCodes: [{ SUCCESS_CODE: [100, 200] }, { FAILURE_CODE: [101, 400] }, { SUSPICIOUS_CODE: [102, 200, 400] }],
-      },
+      { path: '/get-transaction-details/:id', method: 'GET', info: 'get txn details by txn id. the txn id can be found in the response of /make-payment' },
     ],
+    statusCodes: { SUCCESS_CODES: [100, 200], FAILURE_CODES: [101, 400, 401], SUSPICIOUS_CODES: [102, 200, 400] },
   };
 });
 
@@ -64,55 +78,69 @@ fastify.get('/health', async function handler(request, reply) {
   return { status: 'OK' };
 });
 
-fastify.post('/make-payment', async function handler(request, reply) {
-  const { userId, toAccountNumber, fromAccountNumber, amount } = request.body;
-
-  if (validateUuid(userId) === false) {
-    reply.status(400).send({
-      status: 400,
-      message: 'Invalid user id. Must be a uuid v4',
-    });
-  }
-
-  if (toAccountNumber === fromAccountNumber) {
-    reply.status(400).send({
-      status: 400,
-      message: 'Sender and receiver cannot be same',
-    });
-  }
-
-  if (isNaN(amount) || amount <= 100) {
-    reply.status(400).send({
-      status: 400,
-      message: 'Amount must be greater than 100',
-    });
-  }
-
-  const randomCode = _.sample(CODES);
-
-  if (randomCode == FAILURE_CODE) {
-    return {
-      status: FAILURE_CODE,
-      message: 'Could not complete transacation. Service could be unavailable temporarily',
-      txnDetail: JSON.stringify({ txnDetails: null, status: FAILURE_CODE, message: 'Could not complete transacation. Service could be unavailable temporarily' }),
-    };
-  }
-
-  const result = await fastify.db.get(
-    `INSERT INTO transactions (user_id, to_account_number, from_account_number, amount, status, created_date) VALUES (?, ?, ?, ?, ?, ?) RETURNING transaction_id`,
-    [userId, toAccountNumber, fromAccountNumber, amount, randomCode == SUCCESS_CODE, Date.now()]
-  );
-  const [txnDetails] = await fastify.db.all(`SELECT * FROM transactions WHERE transaction_id = ?`, [result.transaction_id]);
-  return {
-    status: SUCCESS_CODE,
-    message: randomCode == SUCCESS_CODE ? 'Transaction completed successfully' : 'Suspicious transaction. Please validate the transaction status with txn Id',
-    txnDetail: JSON.stringify({ ...txnDetails, status: randomCode }),
-  };
+fastify.post('/get-credentials', async function handler(request, reply) {
+  return { username: 'Secret_Username', password: 'Secret_Password' };
 });
 
-fastify.get('/get-transaction-details/:id', async function handler(request, reply) {
-  const txnDetails = await fastify.db.all(`SELECT * FROM transactions WHERE transaction_id = ?`, [request.params.id]);
-  return { ...txnDetails, status: SUCCESS_CODE, message: 'Data fetched successfully' };
+fastify.route({
+  method: 'POST',
+  url: '/make-payment',
+  preHandler: authPreHandler,
+  handler: async function handler(request, reply) {
+    const { userId, toAccountNumber, fromAccountNumber, amount } = request.body;
+
+    if (validateUuid(userId) === false) {
+      reply.status(400).send({
+        status: 400,
+        message: 'Invalid user id. Must be a uuid v4',
+      });
+    }
+
+    if (toAccountNumber === fromAccountNumber) {
+      reply.status(400).send({
+        status: 400,
+        message: 'Sender and receiver cannot be same',
+      });
+    }
+
+    if (isNaN(amount) || amount <= 100) {
+      reply.status(400).send({
+        status: 400,
+        message: 'Amount must be greater than 100',
+      });
+    }
+
+    const randomCode = _.sample(CODES);
+
+    if (randomCode == FAILURE_CODE) {
+      return {
+        status: FAILURE_CODE,
+        message: 'Could not complete transacation. Service could be unavailable temporarily',
+        txnDetail: JSON.stringify({ txnDetails: null, status: FAILURE_CODE, message: 'Could not complete transacation. Service could be unavailable temporarily' }),
+      };
+    }
+
+    const result = await fastify.db.get(
+      `INSERT INTO transactions (user_id, to_account_number, from_account_number, amount, status, created_date) VALUES (?, ?, ?, ?, ?, ?) RETURNING transaction_id`,
+      [userId, toAccountNumber, fromAccountNumber, amount, randomCode == SUCCESS_CODE, Date.now()]
+    );
+    const [txnDetails] = await fastify.db.all(`SELECT * FROM transactions WHERE transaction_id = ?`, [result.transaction_id]);
+    return {
+      status: SUCCESS_CODE,
+      message: randomCode == SUCCESS_CODE ? 'Transaction completed successfully' : 'Suspicious transaction. Please validate the transaction status with txn Id',
+      txnDetail: JSON.stringify({ ...txnDetails, status: randomCode }),
+    };
+  },
+});
+
+fastify.route({
+  method: 'GET',
+  url: '/get-transaction-details/:id',
+  preHandler: authPreHandler,
+  handler: async function handler(request, reply) {
+    const txnDetails = await fastify.db.all(`SELECT * FROM transactions WHERE transaction_id = ?`, [request.params.id]);
+    return { ...txnDetails, status: SUCCESS_CODE, message: 'Data fetched successfully' };
+  },
 });
 
 try {
